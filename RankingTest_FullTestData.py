89715862a09403e809432f989a0e779a325ff2cf
S-D. C <781594648@qq.com>
2020-04-07 17:08:53 +0800

    Update RankingTest_FullTestData.py

--- RankingTest_FullTestData.py
+++ RankingTest_FullTestData.py
@@ -1,52 +1,48 @@
-#!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 """
 @author: Michael (S. Cai)
 """
 
-
 import numpy as np
 
 ### vectors import
 query_vecs_path = 'vectors/SFCANet18_query.npy'
-query_vectors = np.load(query_vecs_path)
+query_vec = np.load(query_vecs_path)
 
 exam_vecs_path = 'vectors/SFCANet18_ref.npy'
-repo_vectors = np.load(exam_vecs_path)
+examing_vec = np.load(exam_vecs_path)
+        
 
+N_data = query_vec.shape[0]
 
-N_data = query_vectors.shape[0]
-### evaluation metrics
-# top 1%
-length = (N_data // 100) + 1
-
-# top-1
-#length = 1
-
-# top-10
-#length = 10
+### keep vector's lentgh
+length = int(N_data * 0.01) + 1
 
 ###
 correct_num = 0
 error_num = 0
-### ranking test
-for i in range(0,N_data):
-    ### comparing
-    diff_QE = query_vectors[i] - repo_vectors
-    ### distance normalization
-    dist_QE = np.linalg.norm(diff_QE, 2, 1)
-    ### distance ranking and telling whether the true match is in the dist_rank
-    dist_rank = np.argsort(dist_QE)[0:length]
-    if(i in dist_rank):
+
+
+dist_E = (examing_vec**2).sum(1).reshape(N_data,1)
+dist_Q = (query_vec**2).sum(1).reshape(N_data,1).T
+
+dist_array = dist_E + dist_Q - 2 * np.matmul(examing_vec, query_vec.T)
+
+
+### ranking and comparing
+for i in range(0, N_data):
+    
+    gt_dist = dist_array[i, i]
+    prediction = np.sum(dist_array[:, i] < gt_dist)
+    if(prediction < length):
         correct_num = correct_num+1
     else:
         error_num = error_num+1
         
-    if(i % 50 == 0):
+    if(i % 1000 == 0):
         print(i)
-        
-        
-### show result of recall at the current evaluation metric
+    
+### recall at top 1%
 result = np.float(correct_num)/np.float(N_data)
 print('result: ', result)
-        
+
