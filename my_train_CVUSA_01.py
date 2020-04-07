41702b26fae871cd72ee36e7fb1815760fe8e3a1
S-D. C <781594648@qq.com>
2020-04-07 17:20:02 +0800

    Update my_train_CVUSA_01.py

--- my_train_CVUSA_01.py
+++ my_train_CVUSA_01.py
@@ -294,11 +294,8 @@ def RankTest(net_test, best_rank_result):
     print('Vec Completed')
         
     ### load vectors
-    query_vectors = query_vec
     
-    repo_vectors = examing_vec
-    
-    N_data = query_vectors.shape[0]
+    N_data = query_vec.shape[0]
     
     ### metrics for evaluation (top 1%)
     length = int(N_data * 0.01) + 1
@@ -312,28 +309,39 @@ def RankTest(net_test, best_rank_result):
     ###
     correct_num = 0
     error_num = 0
+
+
+    dist_E = (examing_vec**2).sum(1).reshape(N_data,1)
+    dist_Q = (query_vec**2).sum(1).reshape(N_data,1)
+
+    ######
+    dpn = 2500
+
     ### ranking and comparing
-    for i in range(0,N_data):
-        ### query vector inflation, N_data times
-        #query_tile = np.tile(query_vectors[i], (N_data,1))
-        ### comparing
-        diff_QE = query_vectors[i] - repo_vectors
-        ### distance norm,  N_data normed dist
-        dist_QE = np.linalg.norm(diff_QE, 2, 1)
-        ### distance ranking whether true match is in the dist_rank
-        dist_rank = np.argsort(dist_QE)[0:length]
-        if(i in dist_rank):
+    for i in range(0, N_data):
+        ### patch k
+        k = int(i / dpn)
+        t = i % dpn
+        if(t==0):
+            dist_Q_h = dist_Q[k*dpn : (k+1)*dpn]
+            dist_array_h = dist_E + dist_Q_h.T - 2 * np.matmul(examing_vec, query_vec[k*dpn : (k+1)*dpn].T)
+            ### calculate   
+        gt_dist = dist_array_h[i, t]
+        prediction = np.sum(dist_array_h[:, t] < gt_dist)
+        if(prediction < length):
             correct_num = correct_num+1
         else:
             error_num = error_num+1
-            
-        if(i % 100 == 0):
+
+
+        if(i % 1000 == 0):
             print(i)
-        
+
     ### recall at top 1%
     result = np.float(correct_num)/np.float(N_data)
     print('result: ', result)
     
+    ###
     net_test.train()
     
     ### restore the best model weights
