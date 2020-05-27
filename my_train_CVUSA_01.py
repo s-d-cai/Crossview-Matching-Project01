db1c372817bc62f738cd7417eb42d6e315bf8f35
S-D. C <781594648@qq.com>
2020-05-28 00:42:23 +0800

    Update my_train_CVUSA_01.py

--- my_train_CVUSA_01.py
+++ my_train_CVUSA_01.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
+
 """
+
 @author: Michael (S. Cai)
 """
 
@@ -14,7 +16,9 @@ import torch.optim as optim
 from torchvision import datasets, models, transforms
 from torch.utils.data import Dataset, DataLoader
 
-from SiamFCANet import SiamFCANet18_CVUSA, SiamFCANet34_CVUSA
+from SiamFCANet import SiamFCANet18_CVUSA, SiamFCANet34_CVUSA # without VLAD layer
+from SiamFCANet_VH import SiamFCANet18_VH, SiamFCANet34_VH
+#from SiamFCANet_VLAD import SiamFCANet18VLAD_CVUSA, SiamFCANet34VLAD_CVUSA # using VLAD layer
 
 from losses_for_training import SoftMargin_TriLoss_OR_UnNorm
 from losses_for_training import HER_TriLoss_OR_UnNorm
@@ -72,7 +76,7 @@ def IsSubString(SubStrList, Str):
     return flag
    
 
-################# triplet data preparation ####################
+################# triplet data preparing ####################
 
 class Triplet_ImageData(Dataset):
     ###label_list 0 1  A means Anchor and P means positive
@@ -146,9 +150,9 @@ class Triplet_ImageData(Dataset):
     
         return trans_img_G, trans_img_S, angle_tensor, angle
 
-#########################
+####### triplet data feeding preparation done
 
-##### for test #####
+##### for testing #####
 
 class ImageDataForExam(Dataset):
     ###label_list 0 1  A means Anchor and P means positive
@@ -179,7 +183,7 @@ class ImageDataForExam(Dataset):
         
         ######################################
         
-        ###### for exam data
+        ###### for examing data
         data_names_sat = os.path.join('', self.image_names_sat[idx])
         image_sat = Image.open(data_names_sat)
         
@@ -195,7 +199,7 @@ class ImageDataForExam(Dataset):
         
         return trans_img_G, trans_img_S
 
-#################
+################# testing data processing done ##############
 
 
 ### load data
@@ -205,19 +209,19 @@ trainIdxList = data.id_idx_list
 testList = data.id_test_list
 testIdxList = data.id_test_idx_list
     
-########################################
-up_root = 'dataset/CVUSA/'
+######################################## data preparation ######################################
+up_root = '/mnt/data/wwq/Crossview_Dataset/CVUSA/dataset/CVUSA/'
 
 ####################### model assignment #######################
 
 #################
 
-net_pre = SiamFCANet18_CVUSA() ### pre model 
+net_pre = SiamFCANet18_VH() ### pre model 
 
-weight_path = 'weights/FCANET18/'
-net_pre.load_state_dict(torch.load(weight_path+'Model_best.pth'))
+weight_path = 'weights/FCANET18_ntest/FT/'
+net_pre.load_state_dict(torch.load(weight_path+'SFCANet_18_VH.pth'))
 
-net_pre_dict = net_pre.state_dict() ### 
+net_pre_dict = net_pre.state_dict() ### given params to a object
 #################
 
 net = SiamFCANet18_CVUSA()
@@ -225,20 +229,23 @@ net.cuda()
 
 net_dict = net.state_dict()
 
-### update net.state_dict() for fine-tune
+### update net.state_dict() for fine-tune training
 net_pre_dict = {k: v for k, v in net_pre_dict.items() if k in net_dict}
 net_dict.update(net_pre_dict)
 net.load_state_dict(net_dict)
 
-save_path = 'weights/FCANET18/'
+save_path = 'weights/FCANET18_ntest/'
 ###########################
 
+###########################
+
+#mini_batch = 9
 mini_batch = 12
 
-### a space to restore loss value
+### assign a vec to restore loss value
 loss_vec = np.zeros(600000, dtype=np.float32)
 
-########################### ranking test (partial test set) ############################
+########################### ranking test ############################
 def RankTest(net_test, best_rank_result):
     ### net evaluation state
     net_test.eval()
@@ -256,20 +263,21 @@ def RankTest(net_test, best_rank_result):
     my_data = ImageDataForExam(filenames_query, filenames_examing)
                                      
     mini_batch = 8
-    testloader = DataLoader(my_data, batch_size=mini_batch, shuffle=False, num_workers=8)
+    testloader = DataLoader(my_data, batch_size=mini_batch, shuffle=False, num_workers=10)
     
     N_data = len(filenames_query)
-    vec_len = 4096
+    #vec_len = 4096
+    vec_len = 1024
     
-    ### N_data % mini_batch
+    ### N_data mini_batch
     nail = N_data % mini_batch
-    ### N_data // mini_batch
+    ### N_data mini_batch
     max_i = N_data // mini_batch
-    ### creat a space for restoring features
+    ### creat a space for restoring the features
     query_vec = np.zeros([N_data,vec_len], dtype=np.float32)
     examing_vec = np.zeros([N_data,vec_len], dtype=np.float32)
     
-    ### feature extraction
+    ### feature extract
     for i, data in enumerate(testloader, 0):
         data_query, data_examing = data
         data_query, data_examing = Variable(data_query).cuda(), Variable(data_examing).cuda()
@@ -291,61 +299,50 @@ def RankTest(net_test, best_rank_result):
             
         print(i)
     
-    print('Vec Completed')
+    print('vec produce done')
         
     ### load vectors
+    query_vectors = query_vec
     
-    N_data = query_vec.shape[0]
+    repo_vectors = examing_vec
     
-    ### metrics for evaluation (top 1%)
+    N_data = query_vectors.shape[0]
+    
+    ### keep vector's lentgh
     length = int(N_data * 0.01) + 1
-    
-    ### top 1
-    #length = 1
-        
-    ### top 10
-    #length = 10
     
     ###
     correct_num = 0
     error_num = 0
+    
 
-    dist_E = (examing_vec**2).sum(1).reshape(N_data,1)
-    dist_Q = (query_vec**2).sum(1).reshape(N_data,1)
-
-    ######
-    dpn = 2500
-
+    dist_E = (examing_vec**2).sum(1).reshape(N_data,1).repeat(N_data,1)
+    dist_Q = (query_vec**2).sum(1).reshape(N_data,1).repeat(N_data,1).T
+    
+    dist_array = dist_E + dist_Q - 2 * np.matmul(examing_vec, query_vec.T)
+    
     ### ranking and comparing
     for i in range(0, N_data):
-        ### patch k
-        k = int(i / dpn)
-        t = i % dpn
-        if(t==0):
-            dist_Q_h = dist_Q[k*dpn : (k+1)*dpn]
-            dist_array_h = dist_E + dist_Q_h.T - 2 * np.matmul(examing_vec, query_vec[k*dpn : (k+1)*dpn].T)
-            ### calculate   
-        gt_dist = dist_array_h[i, t]
-        prediction = np.sum(dist_array_h[:, t] < gt_dist)
+        
+        gt_dist = dist_array[i, i]
+        prediction = np.sum(dist_array[:, i] < gt_dist)
         if(prediction < length):
             correct_num = correct_num+1
         else:
             error_num = error_num+1
-
-
-        if(i % 1000 == 0):
-            print(i)
-
+            
+        #if(i % 100 == 0):
+         #   print(i)
+        
     ### recall at top 1%
     result = np.float(correct_num)/np.float(N_data)
     print('result: ', result)
     
-    ###
     net_test.train()
     
-    ### restore the best model weights
+    ### restore the best params
     if(result > best_rank_result):
-        torch.save(net_test.state_dict(), save_path + 'Model_best.pth')
+        #torch.save(net_test.state_dict(), save_path + 'Model_best.pth')
         np.save(save_path + 'best_result.npy', result)
     
     return result
@@ -356,18 +353,16 @@ def RankTest(net_test, best_rank_result):
 criterion = HER_TriLoss_OR_UnNorm()
 
 
-bestRankResult = 0.8 # initialization of the current best result (randomly setting, 0< num <1)
-
-# loop over the dataset
-N_loop = 100
-for epoch in range(0,N_loop):
+bestRankResult = 0.98457 # current best, Siam-FCANET18
+# loop over the dataset multiple times
+for epoch in range(0,100):
     
     ### save model
 
-    if(epoch>0):
-        compNum = epoch % 5
+    if(epoch>30):
+        compNum = epoch % 1
         print('taking snapshot ...')
-        torch.save(net.state_dict(), save_path + 'model_' + np.str(compNum) + '.pth')
+        #torch.save(net.state_dict(), save_path + 'model_' + np.str(compNum) + '.pth')
         #np.save(save_path + 'loss_vec' + np.str(epoch) + 'epoch.npy', loss_vec)
     
 
@@ -379,21 +374,47 @@ for epoch in range(0,N_loop):
             
     
     if(epoch==0):
-        theta1 = 1.0
-        theta2 = 0.5
+        theta1 = 2.0
+        theta2 = 1.0
     else:
-        theta1 = 1.0
-        theta2 = 0.5
+        theta1 = 5.0
+        theta2 = 2.5
     
 
     net.train()
-    
-    ###
+
+    #base_lr = 0.001
     base_lr = 0.00005
-    base_lr = base_lr*( (1.0-float(epoch)/float(N_loop))**(1.0) )
+    base_lr = base_lr*((1.0-float(epoch)/100.0)**(1.0))
     
     ### 
     optimizer = optim.SGD(net.parameters(), lr=base_lr, momentum=0.9, weight_decay=0.0005)
+    """
+    optimizer = optim.SGD([
+            {'params': net.pre1_conv1.parameters(), 'lr': base_lr},
+            {'params': net.pre2_conv1.parameters(), 'lr': base_lr},
+            {'params': net.pre1_norm1.parameters(), 'lr': base_lr},
+            {'params': net.pre2_norm1.parameters(), 'lr': base_lr},
+            {'params': net.pre1_conv2.parameters(), 'lr': base_lr},
+            {'params': net.pre2_conv2.parameters(), 'lr': base_lr},
+            {'params': net.pre1_norm2.parameters(), 'lr': base_lr},
+            {'params': net.pre2_norm2.parameters(), 'lr': base_lr},
+            {'params': net.pre1_conv3.parameters(), 'lr': base_lr},
+            {'params': net.pre2_conv3.parameters(), 'lr': base_lr},
+            {'params': net.pre1_conv4.parameters(), 'lr': base_lr},
+            {'params': net.pre2_conv4.parameters(), 'lr': base_lr},
+            {'params': net.pre1_conv5.parameters(), 'lr': base_lr},
+            {'params': net.pre2_conv5.parameters(), 'lr': base_lr},            
+            {'params': net.pre1_fc7.parameters(), 'lr': base_lr},
+            {'params': net.pre2_fc7.parameters(), 'lr': base_lr},            
+            {'params': net.pre1_fc8.parameters(), 'lr': base_lr},
+            {'params': net.pre2_fc8.parameters(), 'lr': base_lr},            
+            {'params': net.angle_fc8.parameters(), 'lr': 10*base_lr},
+            {'params': net.angle_fc9.parameters(), 'lr': 10*base_lr},            
+            {'params': net.fc6.parameters(), 'lr': base_lr},
+            {'params': net.fc6_p.parameters(), 'lr': base_lr},  
+        ], lr=base_lr, momentum=0.9, weight_decay=0.00005)
+    """
     optimizer.zero_grad()
     
     ################# txt file and input data preparation ###############
@@ -406,10 +427,10 @@ for epoch in range(0,N_loop):
         grd_list_shuffled.append(rawList[1])
         sat_list_shuffled.append(rawList[0])
     
-    ############ Exemplar list will be shuffled in training ############
+    ############ Anchor and Positive list which will be shuffled during training ############
     num_files = mini_batch * (len(grd_list_shuffled) // mini_batch)
     
-    ### number to control length of data
+    ### repeated number of samples
     repNum = 1
     
     num_files = repNum*num_files
@@ -421,7 +442,7 @@ for epoch in range(0,N_loop):
     triplet_data = Triplet_ImageData(up_root, filenames_grd, filenames_sat)
     
     ### feeding A and P into train loader
-    trainloader = DataLoader(triplet_data, batch_size=mini_batch, shuffle=False, num_workers=8)
+    trainloader = DataLoader(triplet_data, batch_size=mini_batch, shuffle=False, num_workers=10)
     
     for Loop, TripletData in enumerate(trainloader, 0):  
         # get the inputs
